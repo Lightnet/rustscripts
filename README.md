a314253d182720b17fd73d3c93048f208dfddc94
Lightnet <darkneter@gmail.com>
2020-02-03 10:15:43 -0800

    function call from rs to dyon test

--- README.md
+++ README.md
@@ -15,8 +15,26 @@ cargo run scriptlibdyon (lib not command)
 # Workspace:
  Will compile current root workspace. "File Name".dyon will be on workspace when current folder when building.
 
+```
+ scriptappdyon
+  src
+    main.rs (main entory point for rust)
+ game.dyon ( game app)
+ main.dyon ( main entry point for dyon )
+ math.dyon ( lib module)
+ scriptlibdyon (not build yet)
+```
+
 # Note:
- * add function bug name addp to see module setup example works.
+ * add function bug name addp to see module setup example works. "math.dyon"
 
 # Link Refs:
  * http://www.piston.rs/dyon-tutorial/dynamic-modules.html
+ * https://github.com/PistonDevelopers/dyon-tutorial/tree/master/examples/multi_threads
+ * http://www.piston.rs/dyon-tutorial/interop-with-rust.html
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
--- scriptappdyon/src/main.rs
+++ scriptappdyon/src/main.rs
@@ -1,8 +1,36 @@
 
+
+#[macro_use]
 extern crate dyon;
 
-use dyon::{error, run};
+use std::sync::Arc;
+//use dyon::{error, run};
 
 fn main() {
-    error(run("main.dyon"));
+    //error(run("main.dyon"));
+
+    use dyon::{error, Runtime};
+    let mut dyon_runtime = Runtime::new();
+    let dyon_module = load_module().unwrap();
+    if error(dyon_runtime.run(&Arc::new(dyon_module))) {
+        return
+    }
 }
+
+fn load_module() -> Option<dyon::Module> {
+    use dyon::{error, load, Dfn, Module};
+    use dyon::Type::*;
+
+    let mut module = Module::new();
+    module.add_str("say_hello", say_hello, Dfn::nl(vec![], Void));
+
+    if error(load("loader.dyon", &mut module)) {
+        None
+    } else {
+        Some(module)
+    }
+}
+
+dyon_fn!{fn say_hello() {
+    println!("hi! dyon fn!");
+}}
